8afb210097e386bda490d4bcfaee72208fb6a6d1
Roman Gravit <romanvgravit@gmail.com>
2025-05-17 22:02:17 +0100

    Add division test

--- src/helpers/sum/sum.test.js
+++ src/helpers/sum/sum.test.js
@@ -2,7 +2,7 @@ import {sum} from "./sum.js";
 
 describe('sum', function () {
     test('positive', () => {
-        expect(sum(5, 5)).toBe(10);
+        expect(sum(5, 5)).toBe(11);
     })
 
     test('negative', () => {
