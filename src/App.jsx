3e0263b88933990b880c276431d4eeb894a19fc9
Roman Gravit <romanvgravit@gmail.com>
2025-05-17 22:58:00 +0100

    Change color

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
 
     return (
         <div className="App">
-            <h1 style={{fontSize: 70, color: 'red'}}>value = {state}</h1>
+            <h1 style={{fontSize: 70, color: 'blue'}}>value = {state}</h1>
             <button
                 style={{background: 'black', color: "white", padding: 15}}
                 onClick={increment}
